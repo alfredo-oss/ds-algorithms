75a3c8534db437b35897526dcdb87a5dd6543254
alfredo-oss <alfredo.derodt@gmail.com>
2024-04-29 08:40:13 -0400

    Just Added a bunch of comments to the Valid Parentheses Excercise

--- ValidParentheses.py
+++ ValidParentheses.py
@@ -6,26 +6,23 @@ class Solution:
         stack = []
         closeToOpen = { ")" : "(", "]" : "[", "}" : "{"}
 
-        for c in s:
-            if c in closeToOpen:
-                if stack and stack[-1] == closeToOpen[c]:
-                    stack.pop()
-                else:
+        for c in s: # For each character in the given string.
+            print(f"{c}")
+            if c in closeToOpen: # If the character is a key in the HashMap.
+                if stack and stack[-1] == closeToOpen[c]: # Then we check if the stack is empty or not, if its not we concurrently check if the previous value from the stack (referencing the top one) matches the closing value assigned on the HashMap.
+                    print(f"{stack},{closeToOpen[c]}")
+                    stack.pop() # If it matches then we remove the value since it's not a concern anymore. 
+                    print(f"{stack}")
+                else: # Then, "else" handles any other cases like, having elements inside the stack, but not having a matching opening parentheses referencing the HashMap.
                     return False
-            else:
+            else: # If a key does not match the closing characteristics it will be added to the stack
+                print(stack)
                 stack.append(c)
-        return True if not stack else False
+                print(stack)
+        return True if not stack else False # Then we return True if the stack is empty and false if the stack is not since we couldnt find an opening parentheses while referencing the HashMap.
     
 solution = Solution()
-test_1 = "()"
-test_2 = "()[]"
-test_3 = "(]"
+test_1 = "()[]{}"
 
 res = solution.isValid(test_1)
-print(res)
-
-res = solution.isValid(test_2)
-print(res)
-
-res = solution.isValid(test_3)
 print(res)
