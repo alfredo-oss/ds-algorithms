4a4951b76369f719e42baabf4dd9ad03be19f63e
alfredo-oss <alfredo.derodt@gmail.com>
2024-05-10 09:00:09 -0400

    Friday Breakfast

--- RemoveElement.py
+++ RemoveElement.py
@@ -1,17 +1,23 @@
-from typing import List
+"""
+val = 2
+Input: nums = [0,1,2,2,3,0,4,2], val = 2
+Output: 5, nums = [0,1,4,0,3,_,_,_] k = 5
 
+I have to look for numbers that do not correspond to 2, if I find a two, I need a left pointer that tells me where that 2 is, and a right pointer that goes and looks for the next value that is not 2.
+
+"""
+from typing import List, Tuple
 class Solution:
-    def RemoveElement(self, val: int, nums: List[int]) -> int:
+    def RemoveElement(self, arr: List[int], val:int) -> Tuple[List[int], int]:
+
         l = 0
-        for r in range(len(nums)):
-            if nums[r] != val:
-                nums[l] = nums[r]
+
+        for r in range(len(arr)):
+            if val != arr[r]:
+                arr[l] = arr[r]
                 l += 1
-        return l, nums
-    
+        return arr, l
+
 solution = Solution()
 nums = [0,1,2,2,3,0,4,2]
-val = 2
-k , mod_list = solution.RemoveElement(val,nums)
-
-print(f"Unique Values: {k}, Modified List: {mod_list}")
+print(solution.RemoveElement(nums, 2))
