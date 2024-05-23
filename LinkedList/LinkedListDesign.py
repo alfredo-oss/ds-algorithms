a26059f8eb26bd3f14399dbd6a1f995be741c6d7
alfredo-oss <alfredo.derodt@gmail.com>
2024-05-23 06:18:01 -0400

    Thursday Breakfast

--- LinkedList/LinkedListDesign.py
+++ LinkedList/LinkedListDesign.py
@@ -1,3 +1,5 @@
+from typing import List
+
 # Single Linked List Node
 class ListNode:
     def __init__(self, val, next_node=None):
@@ -10,3 +12,41 @@ class LinkedList:
     def __init__(self):
         self.head = ListNode(-1)
         self.tail = self.head
+    
+    def get(self, index: int) -> int:
+        curr = self.head
+        i = 0
+        while i != index:
+            curr = curr.next
+        return curr.val
+    
+    def insertHead(self, val: int) -> None:
+        new_head = ListNode(val)
+        new_head.next = self.head
+        self.head = new_head
+
+    def insertTail(self, val: int) -> None:
+        new_tail = ListNode(val)
+        self.tail.next = new_tail
+        self.tail = new_tail
+
+    def remove(self, index: int) -> bool:
+        curr = self.head
+        i = 0
+        while i < index:
+            curr = curr.next   
+            i += 1
+
+        if curr:
+            curr.next = curr.next.next
+            return True
+        else:
+            return False
+        
+    def getValues(self) -> List[int]:
+        arr = []
+        curr = self.head
+        while curr:
+            arr.append(curr.val)
+            curr = curr.next
+        return arr
