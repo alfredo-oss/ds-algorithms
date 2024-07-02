ba6dedcfdeebc43362582cc98e55b70ca8570542
alfredo-oss <alfredo.derodt@gmail.com>
2024-07-02 07:50:20 -0400

    Key Value Pairs Insertion Sort

---
+++ Sorting/InsertionSort/InsertionSortKVP.py
@@ -0,0 +1,11 @@
+from typing import List
+# Definition of a pair
+class Pair:
+    def __init__(self, key: int, value: str):
+        self.key = key
+        self.value = value
+
+class Solution:
+    def KPInsertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        
+
