241e6760c4d1cb0d190ffa4ef43088195aab9910
alfredo-oss <alfredo.derodt@gmail.com>
2024-07-04 08:16:06 -0400

    Understanding iteration and its bounds

--- Sorting/InsertionSort/InsertionSortKVP.py
+++ Sorting/InsertionSort/InsertionSortKVP.py
@@ -7,15 +7,17 @@ class Pair:
 
 class Solution:
     def KPInsertionSort(self, pairs: List[Pair]) -> List[List[Pair]]:
+        n = len(pairs)
         iter = []
-        for i in range(len(pairs)):
-            j = 0
-            while j >=0 and pairs[j+1][0] < pairs[j][0]:
+
+        for i in range(n):
+            j = i - 1 # The very first element [0] does not enter the iteration because j = -1, hence it does not meet the condition of j >= 0.
+            while j >=0 and pairs[j+1].key < pairs[j].key:
                 temp = pairs[j+1] 
                 pairs[j+1] = pairs[j]
                 pairs[j] = temp
-                iter.append(pairs)
                 j-=1
-            
-            return iter
+            iter.append(pairs) # Iter appending goes here because this is the step after which every comparison for the observed elment has already been done.
+     
+        return iter
 
