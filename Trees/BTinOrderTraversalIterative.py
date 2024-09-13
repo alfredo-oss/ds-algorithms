aa20c0389efea79e14aefb83a25b46ba7c99d2b8
alfredo-oss <alfredo.derodt@gmail.com>
2024-09-13 09:04:49 -0300

    Iterative in order traversal for breakfast

--- Trees/BTinOrderTraversalIterative.py
+++ Trees/BTinOrderTraversalIterative.py
@@ -1,1 +1,23 @@
-from typing import List
+from typing import List, Optional
+
+class TreeNode:
+    def __init__(self, val = 0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur: # This while loop will exit once we reach a null element
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        
+        return res
