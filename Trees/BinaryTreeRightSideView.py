ae5eebc1cabd8e20f1bfc4b9aea7b8e00a7b5536
alfredo-oss <alfredo.derodt@gmail.com>
2024-09-19 11:02:45 -0300

    BinaryTreeRightSide View

--- Trees/BinaryTreeRightSideView.py
+++ Trees/BinaryTreeRightSideView.py
@@ -9,23 +9,22 @@ class TreeNode:
 
 class Solution:
     def rightSideView(self, root:Optional[TreeNode]) -> List[int]:
-
         res = []
         queue = deque()
-
-        if not root:
-            return res
-        
         queue.append(root)
-        res.append(root.val)
-        level = 0
 
-        while len(queue) > 0:
-            print(f"Traversing Level: {level}")
-            for i in range(len(queue)):
-                curr = queue.popleft()
-                if curr.right:
-                    queue.append(curr.right)
-                    res.append(curr.right.val)
-            level += 1
+        while queue:
+            rightSideNode = None
+            qLen = len(queue)
+
+            for i in range(qLen):
+                node = queue.popleft()
+                if node:
+                    rightSideNode = node
+                    queue.append(node.left)
+                    queue.append(node.right)
+
+            if rightSideNode:
+                res.append(rightSideNode.val)
+        
         return res
