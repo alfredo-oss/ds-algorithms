096ba5e4ffaf3b937cf3ca4830d76fbf661b923f
alfredo-oss <alfredo.derodt@gmail.com>
2024-09-19 13:48:03 -0300

    Designing Tree Map

---
+++ Trees/DesignTreeMap.py
@@ -0,0 +1,15 @@
+class TreeMap:
+    
+    def __init__(self):
+        self.key = None
+        self.val = None
+        self.left = None
+        self.right = None
+
+    def insert(self, key: int, val: int) -> None:
+        self.key = key
+        self.val = val
+    
+    def get(self, key: int) -> int:
+        return 
+        
