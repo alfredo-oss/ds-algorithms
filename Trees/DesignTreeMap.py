a589da8ac4fc0d7c58d6a15601941527fe8f5a82
alfredo-oss <alfredo.derodt@gmail.com>
2024-09-20 11:32:28 -0300

    Implementation of TreeMap

--- Trees/DesignTreeMap.py
+++ Trees/DesignTreeMap.py
@@ -1,15 +1,55 @@
-class TreeMap:
-    
-    def __init__(self):
-        self.key = None
-        self.val = None
+class TreeNode:
+    def __init__(self, key:int, val:int):
+        self.key = key
+        self.val = val
         self.left = None
         self.right = None
 
+class TreeMap:
+    def __init__(self):
+        self.root = None
+
     def insert(self, key: int, val: int) -> None:
-        self.key = key
-        self.val = val
+        newNode = TreeNode(key, val)
+        
+        if self.root == None:
+            self.root = newNode
+            return
+        
+        curr = self.root
+        while True:
+            if key < curr.key:
+                if not curr.left:
+                    curr.left = newNode
+                    return
+                curr = curr.left
+                
+            
+            elif key > curr.key:
+                if not curr.right:
+                    curr.right = newNode
+                    return
+                curr = curr.right
+                
+                    
+
+            else:
+                curr.val = val
+                return
     
     def get(self, key: int) -> int:
-        return 
+        curr = self.root
+
+        while True:
+
+            if key < curr.key:
+                curr = curr.left
+
+            elif key > curr.key:
+                curr = curr.right
+
+            else:
+                return curr.val
+            
+        return -1
         
