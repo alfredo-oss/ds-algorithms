7abb044b67f29c371c83317b51cefe26ab26d10d
alfredo-oss <alfredo.derodt@gmail.com>
2024-10-11 08:46:05 -0300

    Kth Largest Element

--- HeapPriorityQueue/KthLargest.py
+++ HeapPriorityQueue/KthLargest.py
@@ -1,7 +1,19 @@
-from HeapImplementation import *
+from MinHeap import *
 
 class KthLargest:
+
     def __init__(self, k: int, nums: list[int]):
+        self.heapmin = Heap()
         self.k = k
-        self.nums = nums
+        self.minHeap = nums
+        self.heapmin.heapify(self.minHeap)
+        while len(self.minHeap) > k:
+            self.heapmin.pop(self.minHeap)
+
+    def add(self, val: int) -> int:
+        self.heapmin.push(self.minHeap, val)
+        if len(self.minHeap) > self.k:
+            self.heapmin.pop(self.minHeap) # we want to pop the "excess" of data
+        return self.minHeap[0]
+
 
