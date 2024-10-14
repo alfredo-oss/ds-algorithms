8123664c1c54a781fe103ba648dc1e559738cd4b
alfredo-oss <alfredo.derodt@gmail.com>
2024-10-14 08:07:57 -0300

    pushing to minheap

--- HeapPriorityQueue/DesignMinHeap.py
+++ HeapPriorityQueue/DesignMinHeap.py
@@ -18,4 +18,11 @@ class MinHeap:
         # once we insert the value into the queue we need to start traversing and formatting the tree so it meets the structure and order properties
         i = len(self.heap) - 1
         
-   
+        # it doesnt matter where we insert the value, the process once its in the Heap its to percolate it up
+        # in the percolate up algorithm the comparison (between the child and parent node) is done in the while statement
+        while i > 1  and self.heap[i] < self.heap[i // 2]:
+            # if the conditions are met we have to swap the values
+            tmp = self.heap[i]
+            self.heap[i] = self.heap[i//2]
+            self.heap[i//2] = tmp
+            i = i//2
