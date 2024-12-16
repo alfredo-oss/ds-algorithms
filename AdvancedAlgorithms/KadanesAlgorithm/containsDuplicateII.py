c48a91f43c65fad36c22470ce333d3792288ddf7
alfredo-oss <alfredo.derodt@gmail.com>
2024-12-16 08:50:42 -0300

    Added hashset version

--- AdvancedAlgorithms/KadanesAlgorithm/containsDuplicateII.py
+++ AdvancedAlgorithms/KadanesAlgorithm/containsDuplicateII.py
@@ -5,7 +5,9 @@ output: bool
 We are given an integer array where we are asked 
 if at different indices of a subarray of length k, 
 we can find the exact same number.
-"""
+
+brute force solution:
+---------------------
 from typing import List
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> int:
@@ -13,4 +15,18 @@ class Solution:
             for R in range(L + 1, min(L + 1 + k, len(nums))):
                 if nums[L] == nums[R]:
                     return True
+        return False
+"""
+from typing import List
+class Solution:
+    def containsNearbyDuplicate(self, nums:List[int], k:int) -> int:
+        window = set()
+        L = 0
+        for R in range(len(nums)):
+            if R - L > k:
+                window.remove(nums[L])
+                L += 1
+            if nums[R] in window:
+                return True
+            window.add(nums[R])
         return False
