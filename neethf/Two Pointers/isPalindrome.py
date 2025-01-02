289c1857fc5dd023ef97a6e12270414eac9d55dd
alfredo-oss <alfredo.derodt@gmail.com>
2025-01-02 11:55:23 -0300

    custom alpha-numeric addition

--- neethf/Two Pointers/isPalindrome.py
+++ neethf/Two Pointers/isPalindrome.py
@@ -13,13 +13,25 @@ class Solution:
         while L < R:
             while L < R and not self.alphaNum(s[L]):
                 L += 1
-            while R > L and not self.alp
+            while R > L and not self.alphaNum(s[R]):
+                R -= 1
+            if s[L].lower() != s[R].lower():
+                return False
+            L += 1
+            R -= 1
+        return True
+    
+    def alphaNum(self, c: str) -> bool:
+        return (ord(A) <= ord(c) <= ord(Z) or
+                ord(a) <= ord(c) <= ord(z) or
+                ord(0) <= ord(c) <= ord(9))
+
 """
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         L, R = 0, len(s)-1
         while L < R:
-            while L < R and not s[L].isalnum():
+            while L < R and not s[L].isalnum(): # the second L < R is a guard because what can happen is that if you don't add it, you could be breaking the condition on a sequence of non-alphanumeric characters. 
                 L += 1
             while R > L and not s[R].isalnum():
                 R -= 1    
