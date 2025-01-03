36200b4d1e07ae6b3b1ed1e2c8a8a12c9872b79b
alfredo-oss <alfredo.derodt@gmail.com>
2025-01-03 07:15:59 -0300

    a little change on the array reference

--- AdvancedAlgorithms/TwoPointers/removeDuplicatesII.py
+++ AdvancedAlgorithms/TwoPointers/removeDuplicatesII.py
@@ -30,7 +30,7 @@ class Solution:
         L, R = 0, 0
         while R < len(nums):
             count = 1 # this count will always initialize when a distinct element is found
-            while R + 1 < len(nums) and nums[R] == R + 1:
+            while R + 1 < len(nums) and nums[R] == nums[R + 1]:
                 count +=1
                 R += 1
             for _ in range(min(2, count)):
