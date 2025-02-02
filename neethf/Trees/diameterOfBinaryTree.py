706d448a37a845bcad0472a44e702b86ec8e7376
alfredo-oss <alfredo.derodt@gmail.com>
2025-02-02 10:14:34 -0300

    diameter of Binary Tree

--- neethf/Trees/diameterOfBinaryTree.py
+++ neethf/Trees/diameterOfBinaryTree.py
@@ -15,3 +15,22 @@ but the catch here is that only roots with 2 nodes can have this consideration.
 the question here is: how do we measure the distance between two nodes that are
 completely separated. we would just sum up their depths...
 """
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res = 0
+        def dfs(root):
+            if not root:
+                return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res = max(res, left + right)
+            return 1 + max(left, right)
+        dfs(root)
+        return res
